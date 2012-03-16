f6cebc92af23c6eda7a63d2a6e63ee2835d17811
jtimberman <joshua@opscode.com>
2012-03-15 20:39:26 -0600

    release v0.10.4

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures tomcat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.3"
+version          "0.10.4"
 
-%w{ java jpackage }.each do |cb|
+%w{ java }.each do |cb|
   depends cb
 end
 
