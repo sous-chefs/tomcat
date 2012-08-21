bbd9af8f47ae3af4a2396d40a17bfd911a719cfa
Phillip Goldenburg <phillip@goldenburg.com>
2012-08-21 15:05:44 -0500

    Update to allow the install of tomcat 7.

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+default["tomcat"]["base_version"] = "6"
 default["tomcat"]["port"] = 8080
 default["tomcat"]["ssl_port"] = 8443
 default["tomcat"]["ajp_port"] = 8009
