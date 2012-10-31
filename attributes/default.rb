30ee25af084bd2df45573abd32a50bd23eb49e7a
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-10-31 12:24:18 -0700

    Allow customizing which IP address(es) Tomcat binds to.
    You might do this if you want Tomcat to listen only on localhost,
    for example.

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,8 +16,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+default["tomcat"]["host"] = '*'
 default["tomcat"]["port"] = 8080
+default["tomcat"]["ssl_host"] = '*'
 default["tomcat"]["ssl_port"] = 8443
+default["tomcat"]["ajp_host"] = '*'
 default["tomcat"]["ajp_port"] = 8009
 default["tomcat"]["java_options"] = "-Xmx128M -Djava.awt.headless=true"
 default["tomcat"]["use_security_manager"] = false
