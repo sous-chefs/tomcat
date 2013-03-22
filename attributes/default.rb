d401abb8b6979423d126db24ac07fa193c658557
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-03-22 10:19:55 -0700

    Support not deploying manager apps

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,6 +24,7 @@ default["tomcat"]["ajp_port"] = 8009
 default["tomcat"]["java_options"] = "-Xmx128M -Djava.awt.headless=true"
 default["tomcat"]["use_security_manager"] = false
 default["tomcat"]["authbind"] = "no"
+default["tomcat"]["deploy_manager_apps"] = true
 default["tomcat"]["ssl_cert_file"] = nil
 default["tomcat"]["ssl_key_file"] = nil
 default["tomcat"]["ssl_chain_files"] = [ ]
