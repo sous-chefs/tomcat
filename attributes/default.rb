c648946a05511169d8c5cee6e93a3306d450f18a
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-03-22 10:17:41 -0700

    Add SSL support

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,6 +24,16 @@ default["tomcat"]["ajp_port"] = 8009
 default["tomcat"]["java_options"] = "-Xmx128M -Djava.awt.headless=true"
 default["tomcat"]["use_security_manager"] = false
 default["tomcat"]["authbind"] = "no"
+default["tomcat"]["ssl_cert_file"] = nil
+default["tomcat"]["ssl_key_file"] = nil
+default["tomcat"]["ssl_chain_files"] = [ ]
+default["tomcat"]["keystore_file"] = "keystore.jks"
+default["tomcat"]["keystore_type"] = "jks"
+default["tomcat"]["keystore_password"] = "ChangeIt"
+default["tomcat"]["truststore_file"] = nil
+default["tomcat"]["truststore_type"] = "jks"
+default["tomcat"]["truststore_password"] = "ChangeIt"
+default["tomcat"]["certificate_dn"] = "cn=localhost"
 default["tomcat"]["loglevel"] = "INFO"
 default["tomcat"]["tomcat_auth"] = "true"
 
@@ -39,6 +49,7 @@ when "centos","redhat","fedora"
   set["tomcat"]["work_dir"] = "/var/cache/tomcat6/work"
   set["tomcat"]["context_dir"] = "#{tomcat["config_dir"]}/Catalina/localhost"
   set["tomcat"]["webapp_dir"] = "/var/lib/tomcat6/webapps"
+  set["tomcat"]["keytool"] = "/usr/lib/jvm/java/bin/keytool"
 when "debian","ubuntu"
   set["tomcat"]["user"] = "tomcat6"
   set["tomcat"]["group"] = "tomcat6"
@@ -50,6 +61,7 @@ when "debian","ubuntu"
   set["tomcat"]["work_dir"] = "/var/cache/tomcat6"
   set["tomcat"]["context_dir"] = "#{tomcat["config_dir"]}/Catalina/localhost"
   set["tomcat"]["webapp_dir"] = "/var/lib/tomcat6/webapps"
+  set["tomcat"]["keytool"] = "/usr/lib/jvm/default-java/bin/keytool"
 else
   set["tomcat"]["user"] = "tomcat6"
   set["tomcat"]["group"] = "tomcat6"
@@ -61,4 +73,5 @@ else
   set["tomcat"]["work_dir"] = "/var/cache/tomcat6"
   set["tomcat"]["context_dir"] = "#{tomcat["config_dir"]}/Catalina/localhost"
   set["tomcat"]["webapp_dir"] = "/var/lib/tomcat6/webapps"
+  set["tomcat"]["keytool"] = "keytool"
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -45,6 +45,14 @@ service "tomcat" do
   action [:enable, :start]
 end
 
+if not node.tomcat["truststore_file"].nil?
+  java_options = node['tomcat']['java_options'].to_s
+  java_options << " -Djavax.net.ssl.trustStore=#{node["tomcat"]["config_dir"]}/#{node["tomcat"]["truststore_file"]}"
+  java_options << " -Djavax.net.ssl.trustStorePassword=#{node["tomcat"]["truststore_password"]}"
+
+  node.set['tomcat']['java_options'] = java_options
+end
+
 case node["platform"]
 when "centos","redhat","fedora"
   template "/etc/sysconfig/tomcat6" do
@@ -78,4 +86,51 @@ template "/etc/tomcat6/logging.properties" do
   group "root"
   mode "0644"
   notifies :restart, "service[tomcat]"
+end
+
+if not node.tomcat.["ssl_cert_file"].nil?
+  cookbook_file "#{node.tomcat.config_dir}/#{node.tomcat.ssl_cert_file}" do
+    mode "0644"
+  end
+  cookbook_file "#{node.tomcat.config_dir}/#{node.tomcat.ssl_key_file}" do
+    mode "0644"
+  end
+  cacerts = ""
+  node.tomcat.ssl_chain_files.each do |cert|
+    cookbook_file "#{node.tomcat.config_dir}/#{cert}" do
+      mode "0644"
+    end
+    cacerts = cacerts + "#{cert} "
+  end
+  script "create_keystore" do
+    interpreter "bash"
+    cwd "#{node.tomcat.config_dir}"
+    code <<-EOH
+      cat #{cacerts} > cacerts.pem
+      openssl pkcs12 -export \
+       -inkey #{node['tomcat']['ssl_key_file']} \
+       -in #{node['tomcat']['ssl_cert_file']} \
+       -chain \
+       -CAfile cacerts.pem \
+       -password pass:#{node['tomcat']['keystore_password']} \
+       -out #{node['tomcat']['keystore_file']}
+    EOH
+    notifies :restart, resources(:service => "tomcat")
+    creates "#{node.tomcat.config_dir}/#{node.tomcat.keystore_file}"
+  end
+else
+  execute "Create SSL certificate" do
+    group node.tomcat.group
+    command "#{node.tomcat.keytool} -genkeypair -keystore \"#{node.tomcat.config_dir}/#{node.tomcat.keystore_file}\" -storepass \"#{node.tomcat.keystore_password}\" -keypass \"#{node.tomcat.keystore_password}\" -dname \"#{node.tomcat.certificate_dn}\""
+    umask 0007
+    creates "#{node.tomcat.config_dir}/#{node.tomcat.keystore_file}"
+    action :run
+    notifies :restart, resources(:service => "tomcat")
+  end
+end
+
+if not node.tomcat["truststore_file"].nil?
+  cookbook_file "#{node['tomcat']['config_dir']}/#{node['tomcat']['truststore_file']}" do
+    mode "0644"
+  end
 end
