9c0e1c4d3934e18f41c65324f10a8b21d15127c7
jtimberman <joshua@opscode.com>
2013-06-09 18:08:47 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures tomcat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.14.0"
+version          "0.14.1"
 
 %w{ java openssl }.each do |cb|
   depends cb
