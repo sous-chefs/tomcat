726cf28ab8c7fd3d5d82839bf1d687715abffc0d
Kamil Bednarz <kami1b4dnarz@gmail.com>
2013-10-07 17:27:14 -0400

    [COOK-3565] Add connectors maxThreads param to the server.xml template
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,6 +27,7 @@ default["tomcat"]["java_options"] = "-Xmx128M -Djava.awt.headless=true"
 default["tomcat"]["use_security_manager"] = false
 default["tomcat"]["authbind"] = "no"
 default["tomcat"]["deploy_manager_apps"] = true
+default["tomcat"]["ssl_max_threads"] = 150
 default["tomcat"]["ssl_cert_file"] = nil
 default["tomcat"]["ssl_key_file"] = nil
 default["tomcat"]["ssl_chain_files"] = [ ]
