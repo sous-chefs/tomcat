0918b0a5fda619a7897b262bcf78396d88c7d60b
Sean OMeara <someara@opscode.com>
2014-03-19 10:38:31 -0400

    Version bump to 0.15.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.15.7'
+version          '0.15.8'
 
 depends 'java'
 depends 'openssl'
