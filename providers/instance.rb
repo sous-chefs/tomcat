92cda731e1a3c28c35cf4eda942d816c6987090e
Tim Ecklund <timothy.ecklund@teamaol.com>
2014-08-18 15:39:51 -0400

    removing tmp log line

--- providers/instance.rb
+++ providers/instance.rb
@@ -154,7 +154,6 @@ action :configure do
     end
   end
 
-  Chef::Log.info "Creating template resource : #{new_resource.config_dir}/server.xml"
   template "#{new_resource.config_dir}/server.xml" do
     source 'server.xml.erb'
       variables ({
