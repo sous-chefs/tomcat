db0f5e2b9f0b3687665dd304c70f4c7f4e573f04
Jonathan <chapmajs@gmail.com>
2014-09-10 16:02:10 -0400

    Setting instance var for server.xml.erb

--- providers/instance.rb
+++ providers/instance.rb
@@ -169,6 +169,7 @@ action :configure do
         :keystore_type => new_resource.keystore_type,
         :tomcat_auth => new_resource.tomcat_auth,
         :config_dir => new_resource.config_dir,
+        :webapp_dir => new_resource.webapp_dir
       })
     owner 'root'
     group 'root'
