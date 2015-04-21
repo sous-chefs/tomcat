9952da51d759b3dbed4270d7dd582c0f1c1c4f93
Sai Boorlagadda <saix.boorlagadda@intel.com>
2015-04-21 10:23:39 -0700

    turned on httponly and secure flag

--- providers/instance.rb
+++ providers/instance.rb
@@ -176,6 +176,14 @@ action :configure do
     notifies :restart, "service[#{instance}]"
   end
 
+  template "#{new_resource.config_dir}/context.xml" do
+    source 'context.xml.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, "service[#{instance}]"
+  end
+
   template "#{new_resource.config_dir}/logging.properties" do
     source 'logging.properties.erb'
     owner 'root'
