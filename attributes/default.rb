4695cf3cd8be6509a5c64031669324455aabf184
David Bernick <dbernick@gmail.com>
2015-06-19 16:31:23 -0400

    issue 153

--- attributes/default.rb
+++ attributes/default.rb
@@ -50,7 +50,7 @@ default['tomcat']['instances'] = {}
 default['tomcat']['run_base_instance'] = true
 default['tomcat']['packages'] = ["tomcat#{node['tomcat']['base_version']}"]
 default['tomcat']['deploy_manager_packages'] = ["tomcat#{node['tomcat']['base_version']}-admin"]
-
+default['tomcat']['ajp_packetsize'] = '8192'
 case node['platform_family']
 
 when 'rhel', 'fedora'
