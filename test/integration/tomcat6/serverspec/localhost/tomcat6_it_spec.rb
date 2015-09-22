7e688382aa3f11ad47b38ec5b84c241edc56b824
Yorgos Saslis <g.saslis@gmail.com>
2015-09-22 12:02:31 +0300

    Added serverspec integration tests for users recipe
    Test that the encrypted data bag item is successfully decrypted and placed in position in the tomcat-users.xml file

--- test/integration/tomcat6/serverspec/localhost/tomcat6_it_spec.rb
+++ test/integration/tomcat6/serverspec/localhost/tomcat6_it_spec.rb
@@ -2,19 +2,24 @@ require 'spec_helper'
 
 service_name = 'tomcat6'
 package_name = 'tomcat6'
+config_dir = '/etc/tomcat'
+
 if host_inventory['platform'] == 'opensuse'
   service_name = 'tomcat'
   package_name = 'tomcat'
-elsif host_inventory['platform'] == 'centos'
+elsif host_inventory['platform'] == 'redhat' # centos is also here
   service_name = 'tomcat6'
   package_name = 'tomcat6'
-elsif os['family'] == 'redhat'
-  service_name = 'tomcat6'
-  package_name = 'tomcat6'
+  config_dir = '/etc/tomcat6'
 elsif host_inventory['platform'] == 'ubuntu'
   service_name = 'tomcat6'
   package_name = 'tomcat6'
+  config_dir = '/etc/tomcat6'
+elsif host_inventory['platform'] == 'debian'
+  service_name = 'tomcat6'
+  package_name = 'tomcat6'
 end
+puts "Platform: #{host_inventory['platform']}, Family: #{os['family']}"
 
 describe package(package_name) do
   it { should be_installed }
@@ -27,4 +32,10 @@ end
 
 describe port(8080) do
   it { should be_listening }
+end
+
+describe file("#{config_dir}/tomcat-users.xml") do
+  its(:content) do
+    should match %r{<user username="testuser" password="testpass" roles="manager-script, admin" \/>}
+  end
 end
--- test/integration/tomcat7/serverspec/localhost/tomcat7_it_spec.rb
+++ test/integration/tomcat7/serverspec/localhost/tomcat7_it_spec.rb
@@ -2,13 +2,19 @@ require 'spec_helper'
 
 service_name = 'tomcat'
 package_name = 'tomcat'
+config_dir = '/etc/tomcat'
+
 if host_inventory['platform'] == 'opensuse'
   service_name = 'tomcat'
   package_name = 'tomcat'
-elsif host_inventory['platform'] == 'centos' || os['family'] == 'redhat'
+elsif host_inventory['platform'] == 'redhat' # centos is also here
   service_name = 'tomcat'
   package_name = 'tomcat'
 elsif host_inventory['platform'] == 'ubuntu'
+  service_name = 'tomcat7'
+  package_name = 'tomcat7'
+  config_dir = '/etc/tomcat7'
+elsif host_inventory['platform'] == 'debian'
   service_name = 'tomcat7'
   package_name = 'tomcat7'
 end
@@ -24,4 +30,10 @@ end
 
 describe port(8080) do
   it { should be_listening }
+end
+
+describe file("#{config_dir}/tomcat-users.xml") do
+  its(:content) do
+    should match %r{<user username="testuser" password="testpass" roles="manager-script, admin" \/>}
+  end
 end
