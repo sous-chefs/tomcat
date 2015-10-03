0bf2124c1adad410206b30411ba631e4fb64fe6c
Joshua C. Burt <jburt@healthwise.org>
2015-10-03 13:45:24 -0600

    old dup removal logic

--- providers/instance.rb
+++ providers/instance.rb
@@ -121,8 +121,7 @@ action :configure do
     java_options << " -Xss#{new_resource.thread_stack_size}" unless new_resource.thread_stack_size.nil? || new_resource.thread_stack_size == ''
     java_options << " -XX:PermSize=#{new_resource.permanent_generation_size}" unless new_resource.permanent_generation_size.nil? || new_resource.permanent_generation_size == ''
     java_options << " -XX:MaxPermSize=#{new_resource.maximum_permanent_generation_size}" unless new_resource.maximum_permanent_generation_size.nil? || new_resource.maximum_permanent_generation_size == ''
-    java_options.strip
-    new_resource.instance_variable_set("@#{:java_options}", java_options_array.join(' ').to_s)
+    new_resource.instance_variable_set("@#{:java_options}", java_options.strip)
   end
 
   unless new_resource.truststore_file.nil?
