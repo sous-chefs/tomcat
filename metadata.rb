7481f2b1d5762b85431be83759ea8cfddb0512f7
Brad Parks <bradleyparks@gmail.com>
2016-02-20 15:39:40 -0600

    No need to roll the version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Apache Tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.1'
 
 depends 'java'
 depends 'openssl'
