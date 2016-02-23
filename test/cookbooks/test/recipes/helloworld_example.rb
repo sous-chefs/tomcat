ddd282f8da4e643785182b65b97d53565c89ec85
Tim Smith <tsmith84@gmail.com>
2016-02-23 15:57:23 -0800

    Extend the test recipe a bit

--- test/cookbooks/test/recipes/helloworld_example.rb
+++ test/cookbooks/test/recipes/helloworld_example.rb
@@ -1,7 +1,9 @@
 tomcat_install 'helloworld' do
   version '8.0.32'
+  exclude_examples false
 end
 
 tomcat_service 'helloworld' do
   action :start
+  env_vars [{ 'CATALINA_PID' => '/opt/tomcat_helloworld/bin/helloworld.pid' }]
 end
