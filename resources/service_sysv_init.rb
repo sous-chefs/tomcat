28451f3f1d1588a4b7b2d1ad7f943ae67738eceb
Tim Smith <tsmith84@gmail.com>
2016-02-24 11:52:36 -0800

    Clarify what versions of debian/ubuntu use sys-v

--- resources/service_sysv_init.rb
+++ resources/service_sysv_init.rb
@@ -4,9 +4,15 @@ provides :tomcat_service, platform: %w(redhat centos scientific oracle) do |node
   node['platform_version'].to_f <= 7.0
 end
 
+provides :tomcat_service, platform: 'debian' do |node|
+  node['platform_version'].to_i < 8
+end
+
+provides :tomcat_service, platform: 'ubuntu' do |node|
+  node['platform_version'].to_f < 15.04
+end
+
 provides :tomcat_service, platform: 'suse'
-provides :tomcat_service, platform: 'debian'
-provides :tomcat_service, platform: 'ubuntu'
 
 property :instance_name, String, name_property: true
 property :path, String, default: nil
