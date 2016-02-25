226dde82eb796b0b41be8e891af59e5323e6b7c5
Tim Smith <tsmith84@gmail.com>
2016-02-25 14:31:08 -0800

    Add the hello world example war to the test
    This better simulates a wrapper cookbook

--- test/cookbooks/test/recipes/helloworld_example.rb
+++ test/cookbooks/test/recipes/helloworld_example.rb
@@ -14,6 +14,13 @@ cookbook_file '/opt/tomcat_helloworld/conf/server.xml' do
   mode '0644'
 end
 
+remote_file '/opt/tomcat_helloworld/webapps/sample.war' do
+  owner 'tomcat_helloworld'
+  mode '0644'
+  source 'https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war'
+  checksum '89b33caa5bf4cfd235f060c396cb1a5acb2734a1366db325676f48c5f5ed92e5'
+end
+
 # start the helloworld tomcat service using a non-standard pic location
 tomcat_service 'helloworld' do
   action :start
