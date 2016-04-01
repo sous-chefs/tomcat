fcdb45abdbf80b41560cff82c24dc35aafe890a3
Tim Smith <tsmith84@gmail.com>
2016-04-01 10:08:32 -0700

    Test the installation of an older version of Tomcat

--- test/cookbooks/test/recipes/docs_example.rb
+++ test/cookbooks/test/recipes/docs_example.rb
@@ -1,16 +1,16 @@
 # make sure we have java installed
 include_recipe 'java'
 
-# Install Tomcat 8.0.32 to a custom path and install the example content / docs
+# Install Tomcat 7.0.44 to a custom path and install the example content / docs
 tomcat_install 'docs' do
-  version '8.0.32'
+  version '7.0.42'
   exclude_examples false
   exclude_docs false
-  install_path '/opt/special/tomcat_docs_8_0_32/'
+  install_path '/opt/special/tomcat_docs_7_0_42/'
 end
 
 # start the tomcat docs install
 tomcat_service 'docs' do
   action [:start, :enable]
-  install_path '/opt/special/tomcat_docs_8_0_32/'
+  install_path '/opt/special/tomcat_docs_7_0_42/'
 end
