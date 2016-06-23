775fae25a10a067aa17b45723bf9c8b818448706
Tim Smith <tsmith@chef.io>
2016-06-22 21:05:46 -0700

    Cookstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/helloworld_example.rb
+++ test/cookbooks/test/recipes/helloworld_example.rb
@@ -25,5 +25,5 @@ end
 # start the helloworld tomcat service using a non-standard pic location
 tomcat_service 'helloworld' do
   action [:start, :enable]
-  env_vars [{ 'CATALINA_PID' => '/opt/tomcat_helloworld/bin/non_standard_location.pid' }, { 'SOMETHING' => 'some_value'}]
+  env_vars [{ 'CATALINA_PID' => '/opt/tomcat_helloworld/bin/non_standard_location.pid' }, { 'SOMETHING' => 'some_value' }]
 end
