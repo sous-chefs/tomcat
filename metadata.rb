d932258530f5996c7e146a6fc48d434b1eae85fa
Tim Smith <tsmith@chef.io>
2016-07-24 11:42:40 -0700

    Fix bad description
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'tomcat'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
-description      'Installs and Apache Tomcat and manages the service'
+description      'Installs Apache Tomcat and manages the service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.3.1'
 
