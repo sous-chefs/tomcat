f1da37d8a0dd2b2902407dfcae882071998d26cb
equick <edwardquick@hotmail.com>
2017-02-06 22:03:14 +0000

    fix for issue #277 to support file scheme

--- resources/install.rb
+++ resources/install.rb
@@ -43,22 +43,30 @@ action_class do
     # preserve the legacy name of sha1_base_path
     new_resource.checksum_base_path = new_resource.sha1_base_path if new_resource.sha1_base_path
     uri = if new_resource.tarball_uri.nil?
-            URI.join(new_resource.checksum_base_path, "tomcat-#{major_version}/v#{new_resource.version}/bin/apache-tomcat-#{new_resource.version}.tar.gz.md5")
+            unless new_resource.tarball_base_path.start_with?('file')
+              URI.join(new_resource.checksum_base_path, "tomcat-#{major_version}/v#{new_resource.version}/bin/apache-tomcat-#{new_resource.version}.tar.gz.md5")
+            else
+              URI.join(new_resource.checksum_base_path, "apache-tomcat-#{new_resource.version}.tar.gz.md5")
+            end
           else
             URI("#{new_resource.tarball_uri}.md5")
           end
-    request = Net::HTTP.new(uri.host, uri.port)
-    response = request.get(uri)
-    if uri.to_s.start_with?('https')
-      request.use_ssl = true
-      request.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
+    if uri.scheme == 'file'
+      open(uri.path).read.split(' ')[0]
+    else
+      request = Net::HTTP.new(uri.host, uri.port)
+      response = request.get(uri)
+      if uri.to_s.start_with?('https')
+        request.use_ssl = true
+        request.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
 
-    if response.code != '200'
-      Chef::Log.fatal("Fetching the Tomcat tarball checksum at #{uri} resulted in an error #{response.code}")
-      raise
+      if response.code != '200'
+        Chef::Log.fatal("Fetching the Tomcat tarball checksum at #{uri} resulted in an error #{response.code}")
+        raise
+      end
+      response.body.split(' ')[0]
     end
-    response.body.split(' ')[0]
   rescue => e
     Chef::Log.fatal("Could not fetch the checksum due to an error: #{e}")
     raise
@@ -83,9 +91,16 @@ action_class do
   def tarball_uri
     uri = ''
     if new_resource.tarball_uri.nil?
-      uri << new_resource.tarball_base_path
-      uri << '/' unless uri[-1] == '/'
-      uri << "tomcat-#{major_version}/v#{new_resource.version}/bin/apache-tomcat-#{new_resource.version}.tar.gz"
+      # comment
+      unless new_resource.tarball_base_path.start_with?('file')
+        uri << new_resource.tarball_base_path
+        uri << '/' unless uri[-1] == '/'
+        uri << "tomcat-#{major_version}/v#{new_resource.version}/bin/apache-tomcat-#{new_resource.version}.tar.gz"
+      else
+        uri << new_resource.tarball_base_path
+        uri << '/' unless uri[-1] == '/'
+        uri << "apache-tomcat-#{new_resource.version}.tar.gz"
+      end
     else
       uri << new_resource.tarball_uri
     end
