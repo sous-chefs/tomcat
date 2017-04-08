a7251bf39a884b6cfd57597b767e1a1715a9fadd
Tim Smith <tsmith@chef.io>
2017-04-08 00:05:41 -0700

    Use standardized license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'tomcat'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs Apache Tomcat and manages the service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.4.0'
