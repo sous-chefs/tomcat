f921b4019a88739c70aa078685d87e46d190b7c7
Tim Smith <tsmith84@gmail.com>
2019-08-19 10:04:22 -0400

    Remove the long_description metadata field
    This doesn't actually do anything
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,9 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs Apache Tomcat and manages the service'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.2.1'
 
-%w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
+%w(ubuntu debian redhat centos suse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
 end
 
