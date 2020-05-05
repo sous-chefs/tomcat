8f46802a82c2f511267d61ee950423be032e74dc
Jason Field <jason@avon-lea.co.uk>
2020-05-05 21:44:21 +0100

    Migration to Sous-Chefs

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,13 @@
-name             'tomcat'
-maintainer       'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license          'Apache-2.0'
-description      'Installs Apache Tomcat and manages the service'
-version          '3.2.2'
+name              'tomcat'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs Apache Tomcat and manages the service'
+source_url        'https://github.com/sous-chefs/tomcat'
+issues_url        'https://github.com/sous-chefs/tomcat/issues'
+chef_version      '>= 13'
+version           '3.3.0'
 
 %w(ubuntu debian redhat centos suse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
 end
-
-source_url 'https://github.com/chef-cookbooks/tomcat'
-issues_url 'https://github.com/chef-cookbooks/tomcat/issues'
-chef_version '>= 13'
