318760610550e27c9bb8747c53582e6b147c2aba
Axel Amigo Arnold <axl89@users.noreply.github.com>
2021-10-21 23:04:52 +0200

    Use OpenJDK 9 on Amazon Linux
    Since it's installed from source, not packages, and 8 is not available
    on the java cookbook from source.

--- test/cookbooks/test/attributes/default.rb
+++ test/cookbooks/test/attributes/default.rb
@@ -1,12 +1,10 @@
 # make sure we get the correct version of java installed
 default['test']['jdk_version'] = '11'
 
-# pin to 8 for some platforms as packages for 11 are unavailable
+# pin to 9 for some platforms as packages for 11 are unavailable
 # either are suitable for testing purposes
-if node.platform?('amazon') ||
-   (
-     node.platform?('debian') && node['platform_version'].to_i <= 9
-   )
-
+if node.platform?('debian') && node['platform_version'].to_i <= 9
   default['test']['jdk_version'] = '8'
+elsif node.platform?('amazon')
+  default['test']['jdk_version'] = '9'
 end
