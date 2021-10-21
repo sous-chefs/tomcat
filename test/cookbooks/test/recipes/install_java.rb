e26dbc347b405869b67e090610461e3c979683e9
Axel Amigo Arnold <axl89@users.noreply.github.com>
2021-10-21 22:39:36 +0200

    Fixed cookbook syntax

--- test/cookbooks/test/recipes/install_java.rb
+++ test/cookbooks/test/recipes/install_java.rb
@@ -1,5 +1,4 @@
-# make sure we have java installed
-openjdk_install do
+openjdk_install "Install OpenJDK Java #{node['test']['jdk_version']}" do
   version node['test']['jdk_version']
   default true
 end
