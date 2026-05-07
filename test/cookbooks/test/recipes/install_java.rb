07e03224aa28e6b9e23f78dfede830751b25c0d9
Dan Webb <dan.webb@damacus.io>
2026-05-07 10:11:36 +0100

    fix: select supported java test packages

--- test/cookbooks/test/recipes/install_java.rb
+++ test/cookbooks/test/recipes/install_java.rb
@@ -1,11 +1,27 @@
 java_package =
-  case node['platform_family']
+  case node['platform']
+  when 'amazon'
+    'java-17-amazon-corretto-devel'
   when 'debian'
-    'openjdk-17-jdk'
-  when 'amazon', 'fedora', 'rhel', 'suse'
+    if node['platform_version'].to_i >= 13
+      'openjdk-21-jdk'
+    else
+      'openjdk-17-jdk'
+    end
+  when 'fedora'
+    'java-21-openjdk-devel'
+  when 'almalinux', 'centos_stream', 'oracle', 'rocky'
+    if node['platform_version'].to_i >= 10
+      'java-21-openjdk-devel'
+    else
+      'java-17-openjdk-devel'
+    end
+  when 'opensuseleap'
+    'java-17-openjdk-devel'
+  when 'redhat'
     'java-17-openjdk-devel'
   else
-    raise "Unsupported platform family for Java install: #{node['platform_family']}"
+    raise "Unsupported platform for Java install: #{node['platform']}"
   end
 
 package java_package
